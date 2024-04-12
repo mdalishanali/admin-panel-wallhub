df89fd04c8e082d99f10c2dd72b3f0fc5f6a0144
mdalishanali <md.alishanali88@gmail.com>
2024-04-12 17:37:34 +0530

    done

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 const { default: axios } = require("axios");
-
+const baseURL = "https://wallpaper-xi.vercel.app/";
 export const api = axios.create({
-  baseURL: "https://server-wallvibes.vercel.app/",
+  // baseURL: "https://server-wallvibes.vercel.app/",
+  baseURL,
 });
